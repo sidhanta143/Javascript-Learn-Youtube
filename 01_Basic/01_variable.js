1527a1fcff152076b5042c29b9418f239347a486
G Sidhanta Dora <sidhantadora143@gmail.com>
2025-11-29 14:17:08 +0000

    data type

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,20 @@
+const acc1="sidha"
+let acc2="1235223"
+var acc3="raja"
+
+acc1="sidhanta"
+acc2="56456454"
+acc3="sdakjfka"
+console.table([acc1,acc2,acc3]
+
+
+//  {----------->This is called scope 
+// }
+ 
+/*
+1.Prefare not use var in your programmer 
+because some isses like block scope and functional scope .
+
+
+
+*/
---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,29 @@
+
+//  "use strict";--->Treat all js code as newer verion.
+
+/*
+alert()---> alert unction is used in browdger but still you use in node.js so this alert functiojn is not for your propose .
+
+
+
+number==>2 to the power 53
+bigint
+string =" "
+boolean==>true /false
+null==>standed alone value
+undefined==>value not assign.
+
+symbol==>unique
+*/ 
+
+
+/**
+ * object
+
+
+
+ * 
+ * 
+ */
+
+
