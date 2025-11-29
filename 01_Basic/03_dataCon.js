15d437066adada261640de5d7de73db76f4f68ec
G Sidhanta Dora <sidhantadora143@gmail.com>
2025-11-29 14:30:43 +0000

    typeconversion

---
+++ 01_Basic/03_dataCon.js
@@ -0,0 +1,21 @@
+
+
+/// Type conversion of data type ---->
+// let num="120 absdfal"
+let num=false
+console.log(typeof(num));
+let val=Number(num)
+console.log(typeof(val));
+console.log(val);
+
+
+/*
+"363"==|>converted in to a number 
+"33afda"==>not converted in to a number or other data type.
+true==>1
+false==|>0
+""-->convert the boolean is false and " sidhan"---->this is show in v=conversion of boolean is true.
+
+
+
+*/
