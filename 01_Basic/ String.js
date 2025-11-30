9c7c534868e0473de2882b83e027da6c39a0631c
G Sidhanta Dora <sidhantadora143@gmail.com>
2025-11-30 06:58:27 +0000

    Sting data type

---
+++ 01_Basic/ String.js
@@ -0,0 +1,12 @@
+
+// let arr={
+//     name:"isdhnata",
+//     age:12
+// }
+// console.log(arr)
+
+
+const name="sidhanta"
+const repo="sidhi"
+// console.log(name+repo)
+console.log("This is my name ${ name}")
