1fc49599391b7ca8c122a0d91f732d0292428b69
G Sidhanta Dora <sidhantadora143@gmail.com>
2025-11-30 03:41:56 +0000

    Data type summary

---
+++ 01_Basic/04_operation.js
@@ -0,0 +1,24 @@
+
+//  operations -------------->
+// let val=100
+// let value=-val
+// console.log(value);
+
+console.log("10"+2);
+console.log(10+"2");
+console.log(10+"2"+10);
+console.log("10"+"2"+10);
+
+
+let a =10
+let b =10
+++a;
+b++;
+console.log(a)
+console.log(b)
+
+// ............................8th class comparision betweeen ..............
+
+// (>,<,<=,>=,==,!=)
+
+
