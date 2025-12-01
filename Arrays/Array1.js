8a1b781e7f5883d15d7ca40d47143f828167a1f4
G Sidhanta Dora <sidhantadora143@gmail.com>
2025-12-01 14:49:53 +0000

    adding

---
+++ Arrays/Array1.js
@@ -0,0 +1,42 @@
+
+// let  arr=[1,22,2,4,8,8,9,10]
+// console.log(arr[3]);
+// console.log(lengt(arr));
+
+// arr.push(100)// Add in to the last of the array.
+// arr.pop() remove the last of the array 
+// console.log((arr));
+
+
+// arr.unshift(200)
+// arr.shift()
+
+
+// console.log(arr.indexOf(22));
+
+
+
+// console.log(arr);
+
+
+
+// **************************  Array2***********************
+
+// const arr1=[12,55,"sidhanta"[3,5,4,7,"raja ",200]]
+// const arr2=[3,5,4,7,"raja ",200]
+//  const arr3=arr1.concat(arr2)
+// console.log(arr3);
+// const arr3=arr1.flat(Infinity)
+// console.log(arr3);
+
+
+
+// let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [4, 5,4,8, 6, 7, 8];
+const newval=[...new Set(arr2)];
+console.log(newval);
+
+
+// let intersection = arr1.filter(value => arr2.includes(value));
+// console.log(intersection); // Output: [4, 5]
+
---
+++ Arrays/Objects.js
@@ -0,0 +1,69 @@
+//  Objects syntex--------->
+// const obj={
+//     name:"sidhanta",
+//     age:12
+// }
+// console.log(obj["name"]);
+// console.log(obj.name);
+
+
+const tinderuser=new Object()
+tinderuser.id="123546";
+tinderuser.name="sidhanta";
+// console.log(tinderuser);
+
+const regularuser={
+    email:"sidhanta@gmail.com",
+    fullname:"sidhanta",
+    age:{
+        age:21,
+
+
+    }
+
+}
+// console.log(regularuser);
+
+
+const ob1={
+    1:"a",
+    2:"b"
+}
+const ob2={
+    3:"d",
+    4:"c"
+}
+// const obj4=Object.assign(ob1,ob2);
+// console.log(obj4);
+const obj4={...ob1,...ob2}
+// console.log(obj4);
+
+
+
+const obj=[
+    {
+    id:1,
+    email:"sihanta@123"
+},
+    {
+    id:1,
+    email:"sihanta@123"
+},
+    {
+    id:1,
+    email:"sihanta@123"
+}
+];
+console.log(obj[1].email);
+
+
+
+
+
+
+
+
+
+
+
+
